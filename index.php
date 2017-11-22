079690318f35a6a3a42825c317a59b309ef906d4
Igor Bushenev <igorbushenev@mail.ru>
2017-11-22 11:55:35 +0300

    second

--- index.php
+++ index.php
@@ -11,27 +11,23 @@
 	</form>
 	<?php 
 		$chisl = $_GET['value'];
-		$per1 = 1;
+		$per1 = 0;
 		$per2 = 1;
 		$per3 = 0;
-		for ($i = 0; $i <= $per1; $i++){
+		while ($per1 != $chisl || $per1 <= $chisl) {
 			if ($per1 > $chisl) {
-				echo $chisl . $per1 . "задуманное число НЕ входит в числовой ряд <br>";
+				echo "<br>задуманное число НЕ входит в числовой ряд";
+				break;
 			}
-			else {
-				if ($per1 == $chisl) {
-				echo $chisl . $per1 . "задуманное число ВХОДИТ в числовой ряд <br>";
+			elseif ($per1 == $chisl) {
+				echo "<br>задуманное число ВХОДИТ в числовой ряд";
+				break;
 				}
 				else {
 					$per3 = $per1;
 					$per1 = $per1 + $per2;
 					$per2 = $per3;
-					echo "<br>" . "переменная  №1--- " . $per1 . "<br>";
-					echo "переменная  №2--- " . $per2 . "<br>";
-					echo "переменная  №3--- " . $per3 . "<br>";
-					echo "вводимое число--- " . $chisl . "<br><br><br>";
 				}
-			}
 		}
 	?>
 </body>
